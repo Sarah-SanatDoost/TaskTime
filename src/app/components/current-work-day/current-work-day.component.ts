fde1b9139eb5bc1a1a31cc797de12e4fb977dac9
sara <sanatdoosts@gmail.com>
2022-10-08 16:36:41 +0330

    try to change color of progress bar for different condition

--- src/app/components/current-work-day/current-work-day.component.ts
+++ src/app/components/current-work-day/current-work-day.component.ts
@@ -13,6 +13,7 @@ export class CurrentWorkDayComponent implements OnInit {
 
   progressbarValue = 0;
   curSec: number = 0;
+  rest: boolean = false;
 
   startTimer(seconds: number) {
     // const time = seconds;
@@ -28,79 +29,22 @@ export class CurrentWorkDayComponent implements OnInit {
     });
   }
 
-  constructor( public dialog: MatDialog ) { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
 
   }
 
-  public onOpenDialog(){
-    this.dialog.open(DialogLeavingWorkComponent ,
+  public onOpenDialog() {
+    this.dialog.open(DialogLeavingWorkComponent,
       { panelClass: 'custom-container' });
   }
-  // timeLeft: number = 60;
-  // interval: string | number | NodeJS.Timer | undefined;
 
-  // startTimer() {
-    // this.interval = setInterval(() => {
-    //   if (this.timeLeft > 0) {
-    //     this.timeLeft--;
-    //   } else {
-    //     this.timeLeft = 60;
-    //   }
-    // }, 1000)
-
-
-  // }
-
-  // setInterval(function () {element.innerHTML += "Hello"}, 1000);
-
+//   public updateColor() {
+// if (this.rest === true){
+//   return 'warn';
+// }else{
+//   return 'primary';
+// }
+//   }
 }
-//    currentDate = new Date();
-//  startOfNextMinute = new Date(
-//   this.currentDate.getFullYear(),
-//   this.currentDate.getMonth(),
-//   this.currentDate.getDate(),
-//   this.currentDate.getHours(),
-//   this.currentDate.getMinutes() + 1
-
-  
-// );
-
-
-//  source = interval(1000);
- 
-//  result = this.source.pipe(
-//   takeUntil(timer(this.startOfNextMinute))
-// );
-
-// }
-// console.log(moment().format('LT'))
-
-
-
-
-
-
-
-
-
-
-
-
-// progressbarValue = 100;
-// curSec: number = 0;
-
-// startTimer(seconds: number) {
-//   const time = seconds;
-//   const timer$ = interval(1000);
-
-//   const sub = timer$.subscribe((sec) => {
-//     this.progressbarValue = 100 - sec * 100 / seconds;
-//     this.curSec = sec;
-
-//     if (this.curSec === seconds) {
-//       sub.unsubscribe();
-//     }
-//   });
-// }
